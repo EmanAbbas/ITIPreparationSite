6cc0dfac857e762361311f620d43b9aef606f27f
abomariam <Eng.Mahmoud.A.Mostafa@gmail.com>
2015-05-09 14:58:17 +0200

    link the SiteUser Model to the django.contrib.auth.models.User model

--- website/new_app/admin.py
+++ website/new_app/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
 from models import *
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 # Register your models here.
 
+class SiteUserInline(admin.StackedInline):
+    model = SiteUser
+    can_delete = False
+    verbose_name_plural = 'Users'
 
-admin.register(Track)
+class SiteUserAdmin(UserAdmin):
+    inlines = (SiteUserInline,)
 
+
+
+admin.site.unregister(User)
+
+admin.site.register(Track)
 admin.site.register(Question)
 admin.site.register(Material)
 admin.site.register(Type)
 admin.site.register(Post)
-admin.site.register(Track)
+admin.site.register(User,SiteUserAdmin)
+
 
--- website/new_app/models.py
+++ website/new_app/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password=models.CharField(max_length=100)
+class SiteUser(models.Model):
+    user = models.OneToOneField(User)
 
     def __str__(self):
-        return self.name
+        return self.user.username
 
 class Track(models.Model):
 	title=models.CharField(max_length=100)
