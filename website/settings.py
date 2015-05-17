186af9e3516e5501375fc29aeacb159bd4d82bd8
Mahmoud Ahmed <eng.mahmoud.a.mostafa@gmail.com>
2015-05-17 09:29:05 +0200

    add SECURE_PROXY to setting.py

--- website/settings.py
+++ website/settings.py
@@ -22,8 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'k82+y5p@rgi-4qcpmk!0iutn4)f!%j4@e_u7@@ryraxog^wh$9'
 
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
