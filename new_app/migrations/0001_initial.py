58bd6e9dd93f5046962b71192d6608e80aa25118
Mahmoud Ahmed <eng.mahmoud.a.mostafa@gmail.com>
2015-06-20 19:23:08 +0200

    Move to MySQLDB

--- new_app/migrations/0001_initial.py
+++ new_app/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
+import new_app.models
 
 
 class Migration(migrations.Migration):
@@ -13,6 +14,17 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Answer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('body', models.TextField(verbose_name=b'Answer')),
+                ('status', models.CharField(default=b'Pending', max_length=10, choices=[(b'Pending', b'Pending'), (b'Approved', b'Approved'), (b'Rejected', b'Rejected')])),
+                ('image', models.ImageField(null=True, upload_to=new_app.models.get_upload_file_name, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('modified', models.DateTimeField(auto_now=True, null=True)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Material',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -20,10 +32,9 @@ class Migration(migrations.Migration):
                 ('link', models.URLField()),
                 ('type', models.CharField(default=b'TRACK', max_length=10, choices=[(b'IQ', b'IQ'), (b'EN', b'English'), (b'TRACK', b'Track')])),
                 ('description', models.TextField(blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('modified', models.DateTimeField(auto_now=True, null=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Post',
@@ -32,22 +43,21 @@ class Migration(migrations.Migration):
                 ('title', models.CharField(max_length=100)),
                 ('body', models.TextField(max_length=1200)),
                 ('date', models.CharField(max_length=100)),
+                ('created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('modified', models.DateTimeField(auto_now=True, null=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Question',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('header', models.CharField(max_length=100)),
-                ('answer', models.TextField(max_length=1200)),
                 ('type', models.CharField(default=b'TRACK', max_length=10, choices=[(b'IQ', b'IQ'), (b'EN', b'English'), (b'FAQ', b'FAQ'), (b'TRACK', b'Track')])),
+                ('status', models.CharField(default=b'Pending', max_length=10, choices=[(b'Pending', b'Pending'), (b'Approved', b'Approved'), (b'Rejected', b'Rejected')])),
+                ('image', models.ImageField(null=True, upload_to=new_app.models.get_upload_file_name, blank=True)),
+                ('created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('modified', models.DateTimeField(auto_now=True, null=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='SiteUser',
@@ -55,9 +65,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Track',
@@ -67,9 +74,6 @@ class Migration(migrations.Migration):
                 ('short_name', models.CharField(max_length=10)),
                 ('description', models.TextField()),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Type',
@@ -77,38 +81,70 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('role_type', models.CharField(max_length=1, choices=[(b'HR', b'HR'), (b'Technical', b'Technical'), (b'Final', b'Final_interview')])),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.AddField(
             model_name='question',
             name='track_id',
-            field=models.ForeignKey(to='new_app.Track'),
-            preserve_default=True,
+            field=models.ManyToManyField(to='new_app.Track', blank=True),
         ),
         migrations.AddField(
             model_name='question',
             name='user_id',
             field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
-            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='voteDownUsers',
+            field=models.ManyToManyField(related_name='questionDownVotes', to=settings.AUTH_USER_MODEL, blank=True),
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='voteUpUsers',
+            field=models.ManyToManyField(related_name='questionUpVotes', to=settings.AUTH_USER_MODEL, blank=True),
         ),
         migrations.AddField(
             model_name='post',
             name='type_name',
             field=models.ForeignKey(to='new_app.Type'),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='material',
             name='track_id',
-            field=models.ManyToManyField(to='new_app.Track'),
-            preserve_default=True,
+            field=models.ManyToManyField(to='new_app.Track', blank=True),
         ),
         migrations.AddField(
             model_name='material',
             name='user_id',
-            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL),
-            preserve_default=True,
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='material',
+            name='voteDownUsers',
+            field=models.ManyToManyField(related_name='materialDownVotes', to=settings.AUTH_USER_MODEL, blank=True),
+        ),
+        migrations.AddField(
+            model_name='material',
+            name='voteUpUsers',
+            field=models.ManyToManyField(related_name='materialUpVotes', to=settings.AUTH_USER_MODEL, blank=True),
+        ),
+        migrations.AddField(
+            model_name='answer',
+            name='question_id',
+            field=models.ForeignKey(related_name='answers', to='new_app.Question'),
+        ),
+        migrations.AddField(
+            model_name='answer',
+            name='user_id',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='answer',
+            name='voteDownUsers',
+            field=models.ManyToManyField(related_name='answerDownVotes', to=settings.AUTH_USER_MODEL, blank=True),
+        ),
+        migrations.AddField(
+            model_name='answer',
+            name='voteUpUsers',
+            field=models.ManyToManyField(related_name='answerUpVotes', to=settings.AUTH_USER_MODEL, blank=True),
         ),
     ]
